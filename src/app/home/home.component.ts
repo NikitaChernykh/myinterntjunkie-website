495ebbe9230d1932976533172f3990adb39501ea
NikitaChernykh <chernykhmain@gmail.com>
2018-05-11 22:37:07 -0400

    added customized bulma to the project

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,7 +3,7 @@ import {trigger,style,transition, animate,keyframes,query, stagger} from '@angul
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css'],
+  styleUrls: ['./home.component.scss'],
   animations: [
     trigger('fadeInAnimation', [
 
