5b46d2c5b52e1ca24f80294120f1d866bc878811
NikitaChernykh <chernykhmain@gmail.com>
2018-06-07 16:49:26 -0400

    added missing server.js

---
+++ server.js
@@ -0,0 +1,10 @@
+
+//Install express server
+var express = require('express');
+var app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist'));
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
