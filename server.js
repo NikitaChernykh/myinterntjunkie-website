6c4bf638dee44e97a4d289585bd2b5cb3349d8e6
NikitaChernykh <chernykhmain@gmail.com>
2018-06-07 16:42:35 -0400

    fixed the layout

--- server.js
+++
@@ -1,9 +0,0 @@
-//Install express server
-var express = require('express');
-var app = express();
-
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
-
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { FeaturesComponent } from './features/features.component';
 import { SubscribeComponent } from './subscribe/subscribe.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
@@ -14,7 +13,8 @@ import { SubscribeComponent } from './subscribe/subscribe.component';
     AppComponent,
     HomeComponent,
     FeaturesComponent,
-    SubscribeComponent
+    SubscribeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
